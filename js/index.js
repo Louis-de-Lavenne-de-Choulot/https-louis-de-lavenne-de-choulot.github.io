b6561ff75fe22cd34c69ddf5bf3738609f648f0b
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-12-22 22:49:18 +0100

    remove and add nav tabs

--- js/index.js
+++ js/index.js
@@ -4,8 +4,31 @@ function Select(elm) {
     selected[0].classList.remove("selected");
     //add class "selected" to elm
     elm.classList.add("selected");
-    //change element with class 'file-title' text to elm text
-    document.getElementsByClassName("file-title")[0].innerText = elm.innerText;
+    // verify in for each elm in .item3 if innerText of elm exists
+    let item3 = document.getElementsByClassName("item3")[0].innerText;
+
+    // slice by \n
+    item3 = item3.split("\n");
+    let exist = false;
+    for (let i = 0; i < item3.length; i++) {
+        if (item3[i] == elm.innerText) {
+            exist = true;
+            break;
+        }
+    }
+    //if not exist create new elm and add class="file-title html" hx-get="innerText" hx-trigger="click" hx-target=".item4" onclick="Select(document.getElementById('file-title'))"
+    if (!exist) {
+        let newElm = document.createElement("div");
+        newElm.innerText = elm.innerText;
+        newElm.classList.add("file-title");
+        newElm.classList.add("html");
+        newElm.setAttribute("hx-get", elm.getAttribute("hx-get"));
+        newElm.setAttribute("hx-trigger", elm.getAttribute("hx-trigger"));
+        newElm.setAttribute("hx-target", ".item4");
+        newElm.setAttribute("onclick", "Select(document.getElementById('home')); this.remove();");
+
+        document.getElementsByClassName("item3")[0].appendChild(newElm);
+    }
 }
 
 function DropDown(elm, elm2) {
