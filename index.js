41c6bd424d5271db343db22bc50baa043b379ba5
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-12-22 18:33:36 +0100

    added responsive

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function Select(elm) {
     selected[0].classList.remove("selected");
     //add class "selected" to elm
     elm.classList.add("selected");
+    //change element with class 'file-title' text to elm text
+    document.getElementsByClassName("file-title")[0].innerText = elm.innerText;
 }
