66c8a3adf3ab38da1975f9ba00d4083f02b43661
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-12-22 20:03:11 +0100

    dropdown added

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function Select(elm) {
     //change element with class 'file-title' text to elm text
     document.getElementsByClassName("file-title")[0].innerText = elm.innerText;
 }
+
+function DropDown(elm, elm2) {
+    //elm ::before rotate 90deg
+    if (elm.classList.contains("rotate")) {
+        elm.classList.remove("rotate");
+        elm.classList.add("rotate-down");
+    } else {
+        elm.classList.add("rotate");
+        elm.classList.remove("rotate-down");
+    }
+    //toggle class "dropdown" on elm2
+    elm2.classList.toggle("dropdown");
+}
