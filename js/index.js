a4ae6888254431c623d3dc8b7d97493cc25d3e18
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-12-23 18:39:23 +0100

    refactor
    optimisation with querySelector

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ let scrollnbrs;
 
 function Select(elm) {
     //search and remove class "selected" from an elm
-    var selected = document.getElementsByClassName("selected");
-    selected[0].classList.remove("selected");
+    var selected = document.querySelector(".selected");
+    selected.classList.remove("selected");
     //add class "selected" to elm
     elm.classList.add("selected");
     // verify in for each elm in .item3 if innerText of elm exists
-    let item3 = document.getElementsByClassName("item3")[0].innerText;
+    let item3 = document.querySelector(".item3").innerText;
 
     // slice by \n
     item3 = item3.split("\n");
@@ -29,7 +29,7 @@ function Select(elm) {
         newElm.setAttribute("hx-target", ".item4");
         newElm.setAttribute("onclick", "Select(document.getElementById('home')); this.remove();");
 
-        document.getElementsByClassName("item3")[0].appendChild(newElm);
+        document.querySelector(".item3").appendChild(newElm);
     }
 }
 
@@ -51,23 +51,21 @@ function DropDown(elm, elm2) {
 
 function Definition(name) {
     //get elm with class "definition"
-    let def = document.getElementsByClassName("definition");
+    let def = document.querySelector(".definition");
     //if elm with class "definition"
-    if (def.length == 1) {
-        //remove class "show" from child of elm with class "definition"
-        for (let i = 0; i < def[0].children.length; i++) {
-            // if elm with class "definition" has child with class "show" and id name is the same as elm with class "definition" child with class "show" remove class "show" and return
-            if (def[0].children[i].classList.contains("show") && def[0].children[i].id == name) {
-                def[0].children[i].classList.remove("show");
-                return;
-            } else {
-                //remove class "show" from elm with class "definition" child
-                def[0].children[i].classList.remove("show");
-            }
+    //remove class "show" from child of elm with class "definition"
+    for (let i = 0; i < def.children.length; i++) {
+        // if elm with class "definition" has child with class "show" and id name is the same as elm with class "definition" child with class "show" remove class "show" and return
+        if (def.children[i].classList.contains("show") && def.children[i].id == name) {
+            def.children[i].classList.remove("show");
+            return;
+        } else {
+            //remove class "show" from elm with class "definition" child
+            def.children[i].classList.remove("show");
         }
-        //add class "show" to elm with id name
-        document.getElementById(name).classList.add("show");
     }
+    //add class "show" to elm with id name
+    document.getElementById(name).classList.add("show");
     // user go to anchor with id name
     window.location.hash = name;
 
